63510cffa794356c4d3f908a40e768efccde402d
FrancesC0de <43376899+FrancesC0de@users.noreply.github.com>
2021-08-15 17:17:25 +0200

    Update cascade_rcnn_hrnetv2p_w32_20e_new_clothes_bal.py

--- configs/hrnet/cascade_rcnn_hrnetv2p_w32_20e_new_clothes_bal.py
+++ configs/hrnet/cascade_rcnn_hrnetv2p_w32_20e_new_clothes_bal.py
@@ -2,7 +2,7 @@
 model = dict(
     type='CascadeRCNN',
     num_stages=3,
-    pretrained='../new_bn.pth',
+    pretrained='new_bn.pth',
     backbone=dict(
         type='HRNet',
         extra=dict(
@@ -257,11 +257,11 @@ optimizer = dict(type='SGD', lr=1e-3, momentum=0.9, weight_decay=0.0001) # 1e-4
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
-    policy='step',
+    policy='cosine',
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=1.0 / 3,
-    step=[16, 19])
+    step=[14, 18])
 checkpoint_config = dict(interval=2)
 # yapf:disable
 log_config = dict(
